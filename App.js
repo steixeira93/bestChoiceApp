970ca1c0671287f497cd646d46ed198d4a852dbd
steixeira93 <steixeira1337@gmail.com>
2022-06-04 12:44:54 -0300

    feat: "some changes"

--- App.js
+++ App.js
@@ -5,17 +5,6 @@ import Home from '../desafioApp/src'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-        <Home/>
-    </View>
+    <Home/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/index.js
+++ src/index.js
@@ -3,34 +3,78 @@ import {
     View,
     Text,
     StyleSheet,
-    TextInput
+    TextInput,
+    Button
 } from 'react-native';
 
 export default function Home() {
 
-    const [etanolValue, setEtanolValue] = useState();
-    const [gasValue, setGasValue] = useState();
+    const [etanolValue, setEtanolValue] = useState('');
+    const [gasValue, setGasValue] = useState('');
 
+    function valorEtanol(price) {
+
+        if(price.length > 0) {
+            setEtanolValue('O valor do etanol na sua cidade é: R$ ' + price)
+        }else{
+            setEtanolValue('')
+        }
+    }
+
+    function valorGasolina(price2) {
+        if(price2.length > 0) {
+            setGasValue('O valor da gasolina na sua cidade é: R$ ' + price2)
+        }else{
+            setGasValue('')
+        }
+    }
+    
     return (
+
         <View style={styles.container}>
         
             <TextInput
                 style={styles.input}
+                placeholder="Qual o valor do Etanol:"
+                onChangeText={ (price) => valorEtanol(price) }
+                keyboardType={'numeric'}
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder="Qual o valor da Gasolina:"
+                onChangeText={ (price2) => valorGasolina(price2) }
+                keyboardType={'numeric'}
+            />
+
+            <Text style={styles.texto}>{etanolValue}</Text>
+            <Text style={styles.texto}>{gasValue}</Text>
+
+            <Button
+                title="Calcular"
             />
 
         </View>
+
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        justifyContent: 'center',
     },
     input: {
-        height: 45,
+        height: 35,
         borderWidth: 1,
-        margin: 10,
-        padding: 17,
-        fontSize: 20
+        margin: 20,
+        padding: 20,
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    texto: {
+        textAlign: 'center',
+        fontSize: 15,
+        margin: 5
     }
 });
